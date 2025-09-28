75e769766cafe4720c271779a7f5fa0e1cbf3193
Zachary Yorks <zyorks.dev@gmail.com>
2025-09-27 21:13:39 -0700

    update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -5,11 +5,4 @@ set -ouex pipefail
 ### Install Cosmic Desktop
 dnf5 install -y @cosmic-desktop-environment
 
-### Enable CachyOS kernel COPR
-dnf5 -y copr enable bieszczaders/kernel-cachyos
-
-# Install CachyOS kernel and headers
-dnf5 install -y kernel-cachyos kernel-cachyos-devel-matched
-
-
 systemctl enable podman.socket
